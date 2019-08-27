9e19beae549ff6b6d3c18eeaf3944170602bf319
Dheeraj Kumar <quic_dhekum@quicinc.com>
2019-08-27 18:35:45 +0530

    DNSMASQ: Updated the dnsmasq start script file
    Updated the start script file to configure similar multiple options
    
    Change-Id: I60992b7b6e07f7f543d088cb1a68752d35517b94
    Signed-off-by: Dheeraj Kumar <quic_dhekum@quicinc.com>
    Acked-by: Nandi Srinivas <snandi@qti.qualcomm.com>

--- recipes/dnsmasq/files/qcmap_start_dnsmasq.sh
+++ recipes/dnsmasq/files/qcmap_start_dnsmasq.sh
@@ -5,17 +5,91 @@ echo "Starting DNSMASQ SERVICE arg1: $1 agr2: ${type_inst} " > $DUMP_TO_KMSG
 
 if [ ${type_inst} == "vlan" ]; then
     echo "launching dnsmasq for tpie vlan inst" > $DUMP_TO_KMSG
-    /usr/bin/dnsmasq --conf-file=${conf_file} \
-    --dhcp-leasefile=${dhcp_leasefile} \
-    --addn-hosts=${addn_hosts} \
-    --pid-file=${pid_file} \
-    --interface=${interface} \
-    --except-interface=lo \
-    -z \
-    --dhcp-range=${dhcp_range} \
-    --dhcp-hostsfile=${dhcp_hostsfile} \
-    --dhcp-option-force=${dhcp_option_force} \
-    --dhcp-script=${dhcp_script}
+    if [ ${#dhcp_option_force3} -ne 0 ];then
+      /usr/bin/dnsmasq --conf-file=${conf_file} \
+      --dhcp-leasefile=${dhcp_leasefile} \
+      --addn-hosts=${addn_hosts} \
+      --pid-file=${pid_file} \
+      --interface=${interface} \
+      --except-interface=lo \
+      -z \
+      --dhcp-range=${dhcp_range} \
+      --dhcp-hostsfile=${dhcp_hostsfile} \
+      --dhcp-option-force=${dhcp_option_force1} \
+      --dhcp-option-force=${dhcp_option_force2} \
+      --dhcp-option-force=${dhcp_option_force3} \
+      --dhcp-script=${dhcp_script}
+    elif [ ${#dhcp_option_force2} -ne 0 ];then
+      /usr/bin/dnsmasq --conf-file=${conf_file} \
+      --dhcp-leasefile=${dhcp_leasefile} \
+      --addn-hosts=${addn_hosts} \
+      --pid-file=${pid_file} \
+      --interface=${interface} \
+      --except-interface=lo \
+      -z \
+      --dhcp-range=${dhcp_range} \
+      --dhcp-hostsfile=${dhcp_hostsfile} \
+      --dhcp-option-force=${dhcp_option_force1} \
+      --dhcp-option-force=${dhcp_option_force2} \
+      --dhcp-script=${dhcp_script}
+    else
+      /usr/bin/dnsmasq --conf-file=${conf_file} \
+      --dhcp-leasefile=${dhcp_leasefile} \
+      --addn-hosts=${addn_hosts} \
+      --pid-file=${pid_file} \
+      --interface=${interface} \
+      --except-interface=lo \
+      -z \
+      --dhcp-range=${dhcp_range} \
+      --dhcp-hostsfile=${dhcp_hostsfile} \
+      --dhcp-option-force=${dhcp_option_force1} \
+      --dhcp-script=${dhcp_script}
+    fi
+elif [ ${type_inst} == "ip_passthrough" ]; then
+    echo "launching dnsmasq for type ippass inst" > $DUMP_TO_KMSG
+    if [ ${#dhcp_option_force3} -ne 0 ];then
+      /usr/bin/dnsmasq --conf-file=${conf_file} \
+      --dhcp-leasefile=${dhcp_leasefile} \
+      --addn-hosts=${addn_hosts} \
+      --pid-file=${pid_file} \
+      --interface=${interface} \
+      --except-interface=lo \
+      -z \
+      --dhcp-range=${dhcp_range1} \
+      --dhcp-hostsfile=${dhcp_hostsfile} \
+      --dhcp-option-force=${dhcp_option_force1} \
+      --dhcp-option-force=${dhcp_option_force2} \
+      --dhcp-option-force=${dhcp_option_force3} \
+      --dhcp-range=${dhcp_range2} \
+      --dhcp-script=${dhcp_script}
+    elif [ ${#dhcp_option_force2} -ne 0 ];then
+      /usr/bin/dnsmasq --conf-file=${conf_file} \
+      --dhcp-leasefile=${dhcp_leasefile} \
+      --addn-hosts=${addn_hosts} \
+      --pid-file=${pid_file} \
+      --interface=${interface} \
+      --except-interface=lo \
+      -z \
+      --dhcp-range=${dhcp_range1} \
+      --dhcp-hostsfile=${dhcp_hostsfile} \
+      --dhcp-option-force=${dhcp_option_force1} \
+      --dhcp-option-force=${dhcp_option_force2} \
+      --dhcp-range=${dhcp_range2} \
+      --dhcp-script=${dhcp_script}
+    else
+      /usr/bin/dnsmasq --conf-file=${conf_file} \
+      --dhcp-leasefile=${dhcp_leasefile} \
+      --addn-hosts=${addn_hosts} \
+      --pid-file=${pid_file} \
+      --interface=${interface} \
+      --except-interface=lo \
+      -z \
+      --dhcp-range=${dhcp_range1} \
+      --dhcp-hostsfile=${dhcp_hostsfile} \
+      --dhcp-option-force=${dhcp_option_force1} \
+      --dhcp-range=${dhcp_range2} \
+      --dhcp-script=${dhcp_script}
+    fi
 elif [ ${type_inst} == "sip_server" ]; then
     /usr/bin/dnsmasq --conf-file=${conf_file} \
     --interface=${interface} \
