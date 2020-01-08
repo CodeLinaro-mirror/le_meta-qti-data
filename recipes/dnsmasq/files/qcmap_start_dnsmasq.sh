a825921910d8f01657b84bf2ff8be4057728fa07
Dheeraj Kumar <quic_dhekum@quicinc.com>
2020-01-08 12:13:55 +0530

    Meta-qti-data : Updated the dnsmasq start script file
    Updated the dnsmasq start script file for ipv6 proxy dns scenario
    Change-Id: Id6bc80cb03d995eca4b10add1ea22aef94920c64
    
    Signed-off-by: Dheeraj Kumar <quic_dhekum@quicinc.com>
    Acked-by: Nandi Srinivas <snandi@qti.qualcomm.com>

--- recipes/dnsmasq/files/qcmap_start_dnsmasq.sh
+++ recipes/dnsmasq/files/qcmap_start_dnsmasq.sh
@@ -90,15 +90,71 @@ elif [ ${type_inst} == "ip_passthrough" ]; then
       --dhcp-range=${dhcp_range2} \
       --dhcp-script=${dhcp_script}
     fi
-elif [ ${type_inst} == "sip_server" ]; then
-    /usr/bin/dnsmasq --conf-file=${conf_file} \
-    --interface=${interface} \
-    --except-interface=lo \
-    -z \
-    --resolv-file=${resolv_file} \
-    --dhcp-range=${dhcp_range} \
-    --dhcp-option=${dhcp_option} \
-    --dhcp-option-force=${dhcp_option_force}
+elif [ ${type_inst} == "dhcpv6_proxy_dns" ]; then
+    echo "launching dnsmasq for type dhcpv6_proxy_dns inst" > $DUMP_TO_KMSG
+    if [ ${#dhcp_option_force4} -ne 0 ];then
+      /usr/bin/dnsmasq --conf-file=${conf_file} \
+      --dhcp-leasefile=${dhcp_leasefile} \
+      --addn-hosts=${addn_hosts} \
+      --pid-file=${pid_file} \
+      --interface=${interface} \
+      --except-interface=lo \
+      -z \
+      --dhcp-range=${dhcp_range1} \
+      --dhcp-hostsfile=${dhcp_hostsfile} \
+      --dhcp-option-force=${dhcp_option_force1} \
+      --dhcp-option-force=${dhcp_option_force2} \
+      --dhcp-option-force=${dhcp_option_force3} \
+      --dhcp-option-force=${dhcp_option_force4} \
+      --dhcp-range=${dhcp_range2} \
+      --dhcp-option=${dhcp_option} \
+      --dhcp-script=${dhcp_script}
+    elif [ ${#dhcp_option_force3} -ne 0 ];then
+      /usr/bin/dnsmasq --conf-file=${conf_file} \
+      --dhcp-leasefile=${dhcp_leasefile} \
+      --addn-hosts=${addn_hosts} \
+      --pid-file=${pid_file} \
+      --interface=${interface} \
+      --except-interface=lo \
+      -z \
+      --dhcp-range=${dhcp_range1} \
+      --dhcp-hostsfile=${dhcp_hostsfile} \
+      --dhcp-option-force=${dhcp_option_force1} \
+      --dhcp-option-force=${dhcp_option_force2} \
+      --dhcp-option-force=${dhcp_option_force3} \
+      --dhcp-range=${dhcp_range2} \
+      --dhcp-option=${dhcp_option} \
+      --dhcp-script=${dhcp_script}
+    elif [ ${#dhcp_option_force2} -ne 0 ];then
+      /usr/bin/dnsmasq --conf-file=${conf_file} \
+      --dhcp-leasefile=${dhcp_leasefile} \
+      --addn-hosts=${addn_hosts} \
+      --pid-file=${pid_file} \
+      --interface=${interface} \
+      --except-interface=lo \
+      -z \
+      --dhcp-range=${dhcp_range1} \
+      --dhcp-hostsfile=${dhcp_hostsfile} \
+      --dhcp-option-force=${dhcp_option_force1} \
+      --dhcp-option-force=${dhcp_option_force2} \
+      --dhcp-range=${dhcp_range2} \
+      --dhcp-option=${dhcp_option} \
+      --dhcp-script=${dhcp_script}
+    else
+      /usr/bin/dnsmasq --conf-file=${conf_file} \
+      --dhcp-leasefile=${dhcp_leasefile} \
+      --addn-hosts=${addn_hosts} \
+      --pid-file=${pid_file} \
+      --interface=${interface} \
+      --except-interface=lo \
+      -z \
+      --dhcp-range=${dhcp_range1} \
+      --dhcp-hostsfile=${dhcp_hostsfile} \
+      --dhcp-option-force=${dhcp_option_force1} \
+      --dhcp-range=${dhcp_range2} \
+      --dhcp-option=${dhcp_option} \
+      --dhcp-script=${dhcp_script}
+    fi
 elif [ ${type_inst} == "relay" ]; then
     /usr/bin/dnsmasq \
     --interface=${interface} \
