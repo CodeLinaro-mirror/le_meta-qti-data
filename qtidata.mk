5826063ea0152c23b366980c06e80db9e350394e
Gayathri S <quic_sgayathr@quicinc.com>
2022-10-27 23:07:43 +0530

    OpenWRT : Enable ipacm, ipanat by default on OpenWRT based on targets
    Enable ipacm, ipanat by default on OpenWRT based on targets
    
    Change-Id: Ie8a58ded178cc5392d822e373597e2399922d23d

--- qtidata.mk
+++ qtidata.mk
@@ -1,12 +1,12 @@
 include $(INCLUDE_DIR)/target.mk
 
-QTIDATA:=rmnetctl libpugixml
+QTIDATA:=rmnetctl libpugixml ipanat ipacm
 
 ###Add target specific packages
 ifeq ($(BOARD),sdx35)
   	QTIDATA+=
 else ifeq ($(BOARD),sdx65)
-  	QTIDATA+= kmod-rmnet-core kmod-rmnet-ctl qps615 ioss qps615-ioss ipanat ipacm
+  	QTIDATA+= kmod-rmnet-core kmod-rmnet-ctl qps615 ioss qps615-ioss
 else ifeq ($(BOARD),sdx75)
   	QTIDATA+=
 endif
