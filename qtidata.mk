ea475dab3b0c1371f5dc10b8be35417894f5c19d
Veena Sri Vallala <quic_vsri@quicinc.com>
2023-07-18 11:21:59 +0530

    meta-qti-data: Add owrt recipe to enable aqc_ioss module
    Added aqc_ioss module.
    Added aqc_ioss init scripts to load the modules when device boots up.
    
    Change-Id: I4dd60ecccab5f90149824699c0666dffe07c3ed9
    Signed-off-by: Veena Sri Vallala <quic_vsri@quicinc.com>

--- qtidata.mk
+++ qtidata.mk
@@ -8,5 +8,5 @@ ifeq ($(BOARD),sdx35)
 else ifeq ($(BOARD),sdx65)
   	QTIDATA+= kmod-rmnet-core kmod-rmnet-ctl qps615 ioss qps615-ioss ipanat ipacm
 else ifeq ($(BOARD),sdx75)
-	QTIDATA+= ipanat ipacm kmod-dataipa kmod-datarmnet kmod-aquantia flashBurn aqr113-firmware kmod-ioss kmod-emac_ioss kmod-gsb kmod-rtsp_alg kmod-rtl8261 datafactory kmod-r8125 avahi-nodbus-daemon kmod-r8125_ioss kmod-sfe
+	QTIDATA+= ipanat ipacm kmod-dataipa kmod-datarmnet kmod-aquantia flashBurn aqr113-firmware kmod-ioss kmod-emac_ioss kmod-gsb kmod-rtsp_alg kmod-rtl8261 datafactory kmod-r8125 avahi-nodbus-daemon kmod-r8125_ioss kmod-sfe kmod-aqc_ioss
 endif
