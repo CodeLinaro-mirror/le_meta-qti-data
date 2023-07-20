ce59738ab773a16f4ca7ad623218605cbb68597b
Ganesh Babu Kamma <quic_gkamma@quicinc.com>
2023-07-20 16:43:47 +0530

    owrt-qti-data: factory reset is failing.
    datafactory module is removed so dhcp, network and firewall config
    is not present for factory reset.
    
    Add datafactory module for compilation

--- qtidata.mk
+++ qtidata.mk
@@ -4,7 +4,7 @@ QTIDATA:=rmnetctl libpugixml
 
 ###Add target specific packages
 ifeq ($(BOARD),sdx35)
-    QTIDATA+= ipanat ipacm kmod-dataipa kmod-datarmnet kmod-ioss kmod-emac_ioss
+    QTIDATA+= ipanat ipacm kmod-dataipa kmod-datarmnet kmod-ioss kmod-emac_ioss datafactory
 else ifeq ($(BOARD),sdx65)
     QTIDATA+= kmod-rmnet-core kmod-rmnet-ctl qps615 ioss qps615-ioss ipanat ipacm
 else ifeq ($(BOARD),sdx75)
