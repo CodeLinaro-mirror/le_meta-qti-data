a01bd7d070df0b349649fda1bbb5ee07c73e74f9
Dheeraj Kumar <quic_dhekum@quicinc.com>
2023-11-27 17:22:18 +0530

    meta-qti-data : KPI path updated in dnsmasq script
    - upon bootup Printing IP assignment KPI for tethered iface
      in KPI Sys node.
    
    Change-Id: I798f4ee1a7bde9c59f9059b7b7f5c83ade16b178
    Signed-off-by:  Dheeraj Kumar<quic_dhekum@quicinc.com>
    Acked-by: Utkal shah <utkashah@qti.qualcomm.com>
    (cherry picked from commit 52a1db05521213730676ed95bc6194572bed5191)

--- recipes/dnsmasq/files/dnsmasq_script.sh
+++ recipes/dnsmasq/files/dnsmasq_script.sh
@@ -46,6 +46,10 @@ fi
 
 case "$1" in
   add)
+      #  Boot KPI marker for Local IP - PLEASE DON'T REMOVE/CHANGE
+      export iface=`bridge fdb show |awk '{print $1,$3,$5}' | grep $2 | grep $DNSMASQ_INTERFACE | awk '{print$2}' | tr -d '\n'`
+      echo -n "M - $iface Client IP $3" >> /sys/kernel/boot_kpi/kpi_values
+      echo "QCMAP: $iface Client Mac Address is $2 and IP Addr is $3" > /dev/kmsg
       #"add" event  means a lease has been created
       #Removing entry for that is already for  ip=$3
       sed -i "/$3/d" $FILE
