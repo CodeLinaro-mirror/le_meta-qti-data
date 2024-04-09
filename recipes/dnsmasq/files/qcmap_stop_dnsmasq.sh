74ed61334120df2d6bd4caa6b14926d7c34959b3
Ganesh Babu Kamma <quic_gkamma@quicinc.com>
2024-04-09 12:44:33 +0530

    meta-qti-data : Resolving dnsmasq prints while bringing up & down XLAT data calls
      * Not able to stop/kill perticular dnsmasq service,
        added a shall script to kill dnsmasq pid for
        default and on-demand bridges.
    
    Change-Id: I5df3458944cd901ab37f490c487da401045a880b
    (cherry picked from commit 03ff77636b46c1528151af1fc76ebc9315efe697)

--- recipes/dnsmasq/files/qcmap_stop_dnsmasq.sh
+++ recipes/dnsmasq/files/qcmap_stop_dnsmasq.sh
@@ -2,5 +2,13 @@
 #Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
 #SPDX-License-Identifier: BSD-3-Clause-Clear
 
-kill -9 `cat /var/run/data/dnsmasq.pid.bridge$1`
+if [ $1 == 0 ]
+then
+    #Kill pid for default bridge
+    kill -9 `cat /var/run/data/dnsmasq.pid`
+else
+    #Kill pid for on-demand bridges
+    kill -9 `cat /var/run/data/dnsmasq.pid.bridge$1`
+fi
 echo "stop dnsmasq "$1 > /dev/kmsg
+
