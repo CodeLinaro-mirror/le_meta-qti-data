569013e0f77f62eea8f744b80aa5ef0aadd5ffd8
Veerendra Kumar Kamsali <quic_veekum@quicinc.com>
2024-05-28 16:16:29 +0530

    owrt-qti-data: disbaling ipacm for sdxbaagha m2 target
    disabling ipacm for sdxbaagha m2 target
    
    Change-Id: I59b6eb1010329928aaaa678cd8c62d1cb9af4156

--- qtidata.mk
+++ qtidata.mk
@@ -5,6 +5,7 @@ QTIDATA:=rmnetctl libpugixml
 ###Add target specific packages
 ifeq ($(BOARD),sdx35)
     QTIDATA+= ipanat ipacm kmod-dataipa kmod-datarmnet kmod-ioss kmod-emac_ioss datafactory kmod-sfe kmod-gsb
+    QTIDATAM2+= ipanat kmod-dataipa kmod-datarmnet kmod-ioss kmod-emac_ioss datafactory kmod-sfe kmod-gsb
 else ifeq ($(BOARD),sdx65)
     QTIDATA+= kmod-rmnet-core kmod-rmnet-ctl qps615 ioss qps615-ioss ipanat ipacm
 else ifeq ($(BOARD),sdx75)
